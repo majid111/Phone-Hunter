0dbd2a858a9eec30688c8f863937864da19ee002
majid111 <abdullahalmajid@gmail.com>
2024-03-03 01:27:41 +0600

    interval

--- Script/Script.js
+++ Script/Script.js
@@ -1,1 +1,29 @@
-console.log('Script added');
+const onload = async () => {
+    const res = await fetch('https://openapi.programming-hero.com/api/phones?search=iphone');
+    const data = await res.json();
+    const phone = data.data;
+    displayPhone(phone);
+    // console.log(phone);
+}
+
+const displayPhone = (phones) => {
+    const CardContainer=document.getElementById('card-container')
+    phones.forEach(phone => {
+        console.log(phone.phone_name);
+        const phoneCard = document.createElement('div');
+        phoneCard.classList = 'card  bg-gray-100 shadow-xl';
+        phoneCard.innerHTML = `
+        <figure><img src="${phone.image}" alt="phones" /></figure>
+        <div class="card-body">
+            <h2 class="card-title">${phone.phone_name}</h2>
+            <p>If a dog chews shoes whose shoes does he choose?</p>
+            <div class="card-actions justify-center">
+            <button class="btn btn-primary text-white">Show Details</button>
+            </div>
+        </div>`;
+
+        CardContainer.appendChild(phoneCard);
+    });
+}
+
+onload();
